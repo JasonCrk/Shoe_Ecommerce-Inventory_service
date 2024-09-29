0c55814c3a977328e47b8c8eab5513f0f9cc838a
JasonCrk <emerzonkolki@gmail.com>
2024-09-28 19:34:24 -0500

    feat: initial commit

---
+++ src/test/java/com/shoe_ecommerce/inventory/TestInventoryApplication.java
@@ -0,0 +1,11 @@
+package com.shoe_ecommerce.inventory;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
