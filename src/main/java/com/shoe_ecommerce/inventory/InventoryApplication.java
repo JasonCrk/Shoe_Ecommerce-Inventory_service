25c8f9327502b10875d0eab285b567a445c3d687
JasonCrk <emerzonkolki@gmail.com>
2024-10-01 17:54:43 -0500

    feat: Service annotation abstraction

--- src/main/java/com/shoe_ecommerce/inventory/InventoryApplication.java
+++ src/main/java/com/shoe_ecommerce/inventory/InventoryApplication.java
@@ -1,9 +1,17 @@
 package com.shoe_ecommerce.inventory;
 
+import com.shoe_ecommerce.inventory.shared.domain.Service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
+		value = { "com.shoe_ecommerce.inventory" }
+)
 public class InventoryApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/shoe_ecommerce/inventory/shared/domain/Service.java
@@ -0,0 +1,9 @@
+package com.shoe_ecommerce.inventory.shared.domain;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface Service {
+}
