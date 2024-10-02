c2bac1c37f983a1e460bed390dd0a5059d6b12a3
JasonCrk <emerzonkolki@gmail.com>
2024-10-02 11:52:09 -0500

    feat(shared/domain): Brand id value object

---
+++ src/main/java/com/shoe_ecommerce/inventory/context/shared/domain/BrandId.java
@@ -0,0 +1,13 @@
+package com.shoe_ecommerce.inventory.context.shared.domain;
+
+import com.shoe_ecommerce.inventory.shared.domain.value_objects.Identifier;
+
+public final class BrandId extends Identifier {
+
+    public BrandId(String value) {
+        super(value);
+    }
+
+    public BrandId() {
+    }
+}
