6480ef054807408938ba5a52c6a5e55a06762683
JasonCrk <emerzonkolki@gmail.com>
2024-10-03 18:44:38 -0500

    feat(category/domain): Add gender field in the category entity

--- src/main/java/com/shoe_ecommerce/inventory/context/category/domain/Category.java
+++ src/main/java/com/shoe_ecommerce/inventory/context/category/domain/Category.java
@@ -1,5 +1,6 @@
 package com.shoe_ecommerce.inventory.context.category.domain;
 
+import com.shoe_ecommerce.inventory.context.category.domain.enums.Gender;
 import com.shoe_ecommerce.inventory.context.category.domain.value_objects.CategoryId;
 import com.shoe_ecommerce.inventory.context.category.domain.value_objects.CategoryName;
 
@@ -8,10 +9,12 @@ import java.util.Objects;
 public final class Category {
     private final CategoryId id;
     private final CategoryName name;
+    private final Gender gender;
 
-    public Category(CategoryId id, CategoryName name) {
+    public Category(CategoryId id, CategoryName name, Gender gender) {
         this.id = id;
         this.name = name;
+        this.gender = gender;
     }
 
     public CategoryId id() {
@@ -22,6 +25,10 @@ public final class Category {
         return name;
     }
 
+    public Gender gender() {
+        return gender;
+    }
+
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
@@ -29,7 +36,9 @@ public final class Category {
         if (object == null || getClass() != object.getClass()) return false;
 
         Category category = (Category) object;
-        return id.equals(category.id) && name.equals(category.name);
+        return id.equals(category.id) &&
+                name.equals(category.name) &&
+                gender.equals(category.gender);
     }
 
     @Override
---
+++ src/main/java/com/shoe_ecommerce/inventory/context/category/domain/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.shoe_ecommerce.inventory.context.category.domain.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    CHILD
+}
