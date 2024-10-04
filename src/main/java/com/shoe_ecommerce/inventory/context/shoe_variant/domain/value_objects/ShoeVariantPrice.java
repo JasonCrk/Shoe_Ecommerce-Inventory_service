143d8ade00d76170999eb4db4d75b8254194eef1
JasonCrk <emerzonkolki@gmail.com>
2024-10-03 20:29:56 -0500

    fix(ShoeVariantPrice.java): Import MoneyValueObject.java

--- src/main/java/com/shoe_ecommerce/inventory/context/shoe_variant/domain/value_objects/ShoeVariantPrice.java
+++ src/main/java/com/shoe_ecommerce/inventory/context/shoe_variant/domain/value_objects/ShoeVariantPrice.java
@@ -1,10 +1,10 @@
 package com.shoe_ecommerce.inventory.context.shoe_variant.domain.value_objects;
 
-import com.shoe_ecommerce.inventory.shared.domain.value_objects.DecimalValueObject;
+import com.shoe_ecommerce.inventory.shared.domain.value_objects.MoneyValueObject;
 
 import java.math.BigDecimal;
 
-public final class ShoeVariantPrice extends DecimalValueObject {
+public final class ShoeVariantPrice extends MoneyValueObject {
 
     public ShoeVariantPrice(BigDecimal value) {
         super(value);
