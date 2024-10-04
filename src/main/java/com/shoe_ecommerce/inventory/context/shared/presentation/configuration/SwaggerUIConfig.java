9f35b418597aeee2333bf212c968f2370cc09e91
JasonCrk <emerzonkolki@gmail.com>
2024-10-03 21:11:13 -0500

    feat(shared/presentation): Swagger UI configuration

---
+++ src/main/java/com/shoe_ecommerce/inventory/context/shared/presentation/configuration/SwaggerUIConfig.java
@@ -0,0 +1,19 @@
+package com.shoe_ecommerce.inventory.context.shared.presentation.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public final class SwaggerUIConfig {
+
+    @Bean
+    public OpenAPI openApiConfig() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Shoe E-commerce - Inventory service")
+                        .version("v1.0.0"));
+    }
+}
