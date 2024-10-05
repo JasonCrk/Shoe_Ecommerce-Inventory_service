1756b6b3ed2736f3b24139bd8da6d64155f88572
JasonCrk <emerzonkolki@gmail.com>
2024-10-05 12:02:11 -0500

    feat(Gender.java): Add min and max sizes

--- src/main/java/com/shoe_ecommerce/inventory/context/category/domain/enums/Gender.java
+++ src/main/java/com/shoe_ecommerce/inventory/context/category/domain/enums/Gender.java
@@ -1,7 +1,18 @@
 package com.shoe_ecommerce.inventory.context.category.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Gender {
-    MALE,
-    FEMALE,
-    CHILD
+    MALE(39, 48),
+    FEMALE(35, 42),
+    CHILD(20, 39);
+
+    private final double minSize;
+    private final double maxSize;
+
+    Gender(double minSize, double maxSize) {
+        this.minSize = minSize;
+        this.maxSize = maxSize;
+    }
 }
