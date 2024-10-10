01f017857910cdc9b1cf0d18bcc398defcfe8ea7
JasonCrk <emerzonkolki@gmail.com>
2024-10-10 12:23:51 -0500

    feat(ShoeInventoryStock.java): Value validation

--- src/main/java/com/shoe_ecommerce/inventory/context/shoe_inventory/domain/value_objects/ShoeInventoryStock.java
+++ src/main/java/com/shoe_ecommerce/inventory/context/shoe_inventory/domain/value_objects/ShoeInventoryStock.java
@@ -5,6 +5,12 @@ import com.shoe_ecommerce.inventory.shared.domain.value_objects.IntValueObject;
 public final class ShoeInventoryStock extends IntValueObject {
 
     public ShoeInventoryStock(int value) {
-        super(value);
+        super(ensureIsValid(value));
+    }
+
+    private static int ensureIsValid(int value) throws IllegalArgumentException {
+        if (value < 0) throw new IllegalArgumentException("Stock cannot be negative");
+
+        return value;
     }
 }
