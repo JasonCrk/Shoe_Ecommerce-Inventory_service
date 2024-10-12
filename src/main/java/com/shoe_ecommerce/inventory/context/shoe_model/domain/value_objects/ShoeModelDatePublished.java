a2a4828bf3bc69695b3985dfec86d10f8cdeb2d3
JasonCrk <emerzonkolki@gmail.com>
2024-10-12 17:14:29 -0500

    feat(ShoeModelDatePublished.java): Now method

--- src/main/java/com/shoe_ecommerce/inventory/context/shoe_model/domain/value_objects/ShoeModelDatePublished.java
+++ src/main/java/com/shoe_ecommerce/inventory/context/shoe_model/domain/value_objects/ShoeModelDatePublished.java
@@ -3,6 +3,8 @@ package com.shoe_ecommerce.inventory.context.shoe_model.domain.value_objects;
 import com.shoe_ecommerce.inventory.shared.domain.value_objects.DateValueObject;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public final class ShoeModelDatePublished extends DateValueObject {
 
@@ -12,5 +14,9 @@ public final class ShoeModelDatePublished extends DateValueObject {
 
     public ShoeModelDatePublished() {
         super(null);
+    }
+
+    public static ShoeModelDatePublished now() {
+        return new ShoeModelDatePublished(ZonedDateTime.now(ZoneId.of("UTC")).toLocalDate());
     }
 }
